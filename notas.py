9048005c7b11b2cea3b576217d4d0ee507459504
Dvd-coder-art <daviddavi1221@gmail.com>
2025-04-10 08:19:41 -0300

    Subindo arquivos do projeto students crud

--- notas.py
+++ notas.py
@@ -1,13 +1,16 @@
 import os
 import re
 import mysql.connector
+from dotenv import load_dotenv 
+
+load_dotenv
 
 def conectar():
     return mysql.connector.connect(
         host='localhost',
-        user="root",
-        password="root",
-        database="bdpython"
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASS'),
+        database=os.getenv('DB_NAME')
         )
 
 
